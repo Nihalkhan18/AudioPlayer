89b1a96c7773009b3e020e2ba8e46ba810cce54f
Nihalkhan18 <131291695+Nihalkhan18@users.noreply.github.com>
2024-02-21 19:03:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import AudioPlayer from './Components/AudioPlayer';
+
+function App() {
+  
+  return (
+    <div className="App">
+
+      <AudioPlayer  />
+    </div>
+  );
+}
+
+export default App;
